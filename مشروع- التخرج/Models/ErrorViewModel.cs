2a9f4e71cf2fc9cf1ec85df6281684a6a39e6039
Taima <taimaalsafeen.2020@gmail.com>
2026-03-30 17:07:15 +0300

    Add project files.

---
+++ مشروع- التخرج/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace مشروع__التخرج.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
